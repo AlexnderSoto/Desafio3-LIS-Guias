8114434e2734e8957d4386a16f13f0972d431827
JonathanRodriguezC <89150664+JonathanRodriguezC@users.noreply.github.com>
2024-05-07 16:51:41 -0600

    Guia1

---
+++ Guia1_RC181256/partehtmlphp.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title> Scripts con (X)HTML y PHP </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" meida="screen" href="css/htmlphp.css">
+    <script src="js/modernizr.custom.lis.js"></script>
+    <script src="js/prefixfree.min.js"></script>
+</head>
+
+<body>
+    <div class="wrap">
+        <header>
+            <h1>Demostracion de HTML y PHP</h1>
+        </header>
+        <!-- Parte de la pagina web generada con HTML -->
+        <section class="main">
+            <article id="html">
+                <div class="text">
+                    <p>
+                        &lt;!DOCTYPE html&gt; <br />
+                        &lt;html &gt; <br />
+                        ...
+                    </p>
+                    <p>Parte de HTML normal.</p>
+                    <p>
+                        &lt;/html &gt;
+                    </p>
+                </div>
+                <div class="img">
+                    <img src="img/html5.png" alt="Logo de HTML5">
+                </div>
+            </article>
+            <?php
+            //Parte de la pagina generada con lenguaje PHP
+            echo "<article id=\"php\">\n";
+            echo "<div class=\"text\">";
+            echo "<p>&lt;?php<br />";
+            echo "...<br />";
+            echo "Parte con PHP. </p>";
+            echo "<p>?&gt;</p>";
+            echo "</div>";
+            echo "<div class=\"img\">";
+            echo "<img src=\"img/php.png\" alt=\"Logo de HTML5\" />";
+            echo "</div>";
+            echo "\n</article>\n";
+            ?>
+        </section>
+</body>
+<!-- Fin del div class="wrap" -->
+</div>
+
+</html>
